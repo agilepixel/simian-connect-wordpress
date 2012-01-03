c362c69337e0dc540dfcfb0b05938ade33ea5edf
Henry Allsuch <henry@thecodepharmacy.co.uk>
2012-01-03 02:39:16 +0000

    • Set scaling in js so user can control poster + movie dimensions.
    git-svn-id: https://xp-dev.com/svn/simian/trunk@7 783836f2-1074-48e8-bc95-db944424cdfd

--- simianconnect/js/simian.js
+++ simianconnect/js/simian.js
@@ -1,8 +1,14 @@
-
-
 //QT posters
 if (typeof(QTP) != "undefined" && typeof(QTP.Poster) != "undefined") { 
 QTP.Poster.prototype.clickText = "Click To Play";
+
+QTP.Poster.prototype.attributes = {
+		controller: 'false',
+		autoplay: 'true', 
+		bgcolor: 'black', 
+		scale: 'tofit',
+		postdomevents: 'true'
+	};
 }
 
 //reel movie loading
@@ -19,7 +25,7 @@ $j(document).ready(function() {
 		
 		$p.find('div.reelVideo').empty().append($m);
 		
-		qtEmbed($reel_id +"_mov",$j(this).attr('href'),640,360,"true");
+		qtEmbed($reel_id +"_mov",$j(this).attr('href'),480,240,"true");
 		
 		$p.find('.mediaTitle').html($j(this).children('img').first().attr('title'));
 		
@@ -31,27 +37,11 @@ $j(document).ready(function() {
 
 function qtEmbed($dom_id,$src,$width,$height,$autostart){
 
-	//$j(document).ready(function(){
-	
 	if (typeof(QT) != "undefined") { 
 	
 		var $new = QT.GenerateOBJECTText_XHTML($src, $width, $height, '','scale','tofit', 'autostart',$autostart);
 		
 		$($dom_id).replace($new);
 		
-	} else {
-	
-		console.log(QT);
-	
 	}
-	
-	//});
-
-}
-
-function htmlembed(url,width,height){
-	
-	html = "<object classid=\"clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B\" codebase=\"http://www.apple.com/qtactivex/qtplugin.cab\" height=\"256\" width=\"320\"><param name=\"src\" value=\""+url+"\"><param name=\"autoplay\" value=\"false\"><param name=\"type\" value=\"video/quicktime\" height=\""+height+"\" width=\""+width+"\"><embed src=\""+url+"\" height=\""+height+"\" width=\""+width+"\" autoplay=\"false\" type=\"video/quicktime\" pluginspage=\"http://www.apple.com/quicktime/download/\"></object>";
-
-	return html;
 }
